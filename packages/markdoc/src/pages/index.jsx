e4a3e66a5c65a74881dd41f74a71f41310f8cd8a
agnelnieves <agnelnieves@gmail.com>
2023-07-16 11:30:54 -0400

    feat(governance-docs): add showcase section

--- packages/markdoc/src/pages/index.jsx
+++ packages/markdoc/src/pages/index.jsx
@@ -5,6 +5,7 @@ import { navigation } from '@/data/navigation'
 import { features } from '@/data/home'
 
 import { MobileNavigation } from '@/components/MobileNavigation'
+import Showcase from '@/components/Showcase'
 
 import Link from 'next/link'
 
@@ -215,14 +216,17 @@ export default function Home() {
                         Shape the future of your projects through transparent and inclusive
                         governance processes.
                     </p>
+                    <div className='flex flex-wrap gap-2.5'>
+                        <Link href="#features" class="rounded-md bg-teal-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-400">Get started</Link>
+                        <Link href="#features" class="rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400">Get started</Link>
+                    </div>
                 </div>
             </section>
 
-            <section className="z-10 my-10 gap-6 px-8">
+            <section id="features" className="z-10 mt-10 py-24 sm:py-32 gap-6 px-8">
                 <div className="mx-auto max-w-xl sm:text-center mb-12">
                     <h2 className="text-base font-semibold leading-7 text-teal-400">Features at glance</h2>
                     <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                        {/* Catchy title */}
                         No DAO? No problem.
                     </p>
                     <p className="mt-4 text-lg leading-8 text-gray-300">Tweak parameters, upgrade smart contracts and manage your treasury with the flexibility to build your own governance model.</p>
@@ -239,12 +243,15 @@ export default function Home() {
                     ))}
                 </div>
             </section>
+
+            <Showcase />
             <div className='p-8 pt-0'>
                 <Footer />
             </div>
         </div>
     )
 }
+
 
 function FeatureCard(props) {
     return (
