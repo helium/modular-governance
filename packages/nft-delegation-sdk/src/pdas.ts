08eca226a45f9ce1721b9eca7c5938f08d9d48dc
Chewing Glass <chewinglass123@gmail.com>
2023-11-16 15:47:56 -0600

    Add delegation config key

--- packages/nft-delegation-sdk/src/pdas.ts
+++ packages/nft-delegation-sdk/src/pdas.ts
@@ -1,15 +1,29 @@
 import { PublicKey } from "@solana/web3.js";
 import { PROGRAM_ID } from "./constants";
 
-export function delegationKey(delegationConfig: PublicKey, mint: PublicKey, owner: PublicKey, programId: PublicKey = PROGRAM_ID): [PublicKey, number] {
+export function delegationConfigKey(
+  name: string,
+  programId: PublicKey = PROGRAM_ID
+): [PublicKey, number] {
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("delegation_config", "utf-8"), Buffer.from(name, "utf8")],
+    programId
+  );
+}
+
+export function delegationKey(
+  delegationConfig: PublicKey,
+  mint: PublicKey,
+  owner: PublicKey,
+  programId: PublicKey = PROGRAM_ID
+): [PublicKey, number] {
   return PublicKey.findProgramAddressSync(
     [
       Buffer.from("delegation", "utf-8"),
       delegationConfig.toBuffer(),
       mint.toBuffer(),
-      owner.toBuffer()
+      owner.toBuffer(),
     ],
     programId
   );
 }
-
