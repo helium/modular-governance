fb5391d1362b13fe016d3cc9db296832ab81ca79
bry <bryanzettler@gmail.com>
2024-08-07 11:31:14 -0500

    remove unneeded account

--- programs/nft_proxy/src/instructions/close_expired_proxy_v0.rs
+++ programs/nft_proxy/src/instructions/close_expired_proxy_v0.rs
@@ -12,14 +12,12 @@ pub struct CloseExpiredProxyV0<'info> {
   #[account(
     mut,
     close = rent_refund,
-    has_one = proxy_config,
     has_one = rent_refund,
     constraint = proxy_assignment.index == 0,
     constraint = proxy_assignment.next_voter == Pubkey::default(),
     constraint = proxy_assignment.expiration_time < Clock::get().unwrap().unix_timestamp @ ErrorCode::ExpirationNotPast,
   )]
   pub proxy_assignment: Box<Account<'info, ProxyAssignmentV0>>,
-  pub proxy_config: Box<Account<'info, ProxyConfigV0>>,
   pub system_program: Program<'info, System>,
 }
 
